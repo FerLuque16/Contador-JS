86fa1bfce38ba8ea851225d7ffc794057746ba1c
FerLuque16 <feerluque77@gmail.com>
2022-01-30 01:05:23 -0300

    Cree la funcionalidad y un poco de estilo, falta mejorarle el estilo y tal vez agregar mas contadores a traves de un boton

---
+++ Contador/js/scripts.js
@@ -0,0 +1,25 @@
+let numero = parseInt(document.querySelector("#numero").textContent);
+
+const incremento = document.querySelector("#incremento");
+const decremento = document.querySelector("#decremento");
+const reincicio = document.querySelector("#reinicio");
+
+incremento.addEventListener("click",()=>{
+    numero++;
+    document.querySelector("#numero").textContent = numero;
+})
+decremento.addEventListener("click",()=>{
+    numero--;
+    document.querySelector("#numero").textContent = numero;
+})
+
+reincicio.addEventListener("click", ()=>{
+    numero = 0;
+    document.querySelector("#numero").textContent = numero;
+})
+
+
+
+console.log(typeof(numero));
+console.log(decremento);
+console.log(incremento);
